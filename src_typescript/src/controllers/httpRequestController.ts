78f74e2f34b3e6b65bd94128fe191c9e0dd062aa
Leonhard Gahr <leonhard.gahr@gmail.com>
2021-06-23 18:36:18 +0200

    let the earth burn

--- src_typescript/src/controllers/httpRequestController.ts
+++ src_typescript/src/controllers/httpRequestController.ts
@@ -3,14 +3,15 @@ import axios from 'axios';
 
 export default class HttpRequestController {
     public static async makeRequest(url: string, basicAuthToken: string, method: string) {
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
         const agent = new https.Agent({
             rejectUnauthorized: false
         });
+        axios.defaults.httpsAgent = agent;
 
         switch (method.toUpperCase()) {
             case 'POST':
                 await axios.post(url, {
-                    httpsAgent: agent,
                     headers: {
                         Authorization: basicAuthToken
                     }
@@ -18,7 +19,6 @@ export default class HttpRequestController {
                 break;
             case 'GET':
                 await axios.post(url, {
-                    httpsAgent: agent,
                     headers: {
                         Authorization: basicAuthToken
                     }
